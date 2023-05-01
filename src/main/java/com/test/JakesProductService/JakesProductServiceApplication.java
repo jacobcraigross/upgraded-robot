ff236f97615b934d31d8a355b21fc07a3f1fd782
jacobcraigross <jakerossdev@gmail.com>
2023-05-01 05:10:14 -0500

    initial commit

---
+++ src/main/java/com/test/JakesProductService/JakesProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.JakesProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakesProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakesProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/JakesProductService/JakesProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.JakesProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakesProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
